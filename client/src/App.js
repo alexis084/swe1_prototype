0a0560a70d483b85414127000e003388cfac9488
alexis084 <alexus084@gmail.com>
2023-11-13 22:57:28 -0700

    adding state to event creation form

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
   const [eventName, setEventName] = useState('');
-  const [eventTimeStampe, setEventTimeStamp] = useState('');
+  const [eventTimeStamp, setEventTimeStamp] = useState('');
   const [eventDescription, setEventDescription] = useState('');
   
 
@@ -15,18 +15,38 @@ function App() {
         <input
           type="text"
           required
+          value={eventName}
+          onChange={
+            (userUpdateEventName) => setEventName(
+              userUpdateEventName.target.value)
+          }
         />
         <label>Enter Event Time Stamp</label>
         <input
           type="text"
           required
+          value={eventTimeStamp}
+          onChange={
+            (userUpdateEventTimeStamp) => setEventTimeStamp(
+              userUpdateEventTimeStamp.target.value)
+          }
         />
         <label>Enter Event Description</label>
         <textarea
-          required  
+          required
+          value={eventDescription}
+          onChange={
+            (userUpdateEventDescription) => setEventDescription(
+              userUpdateEventDescription.target.value)
+          }  
         ></textarea>
         <button>Create Event</button>
       </form>
+      
+      <p>{eventName}</p>
+      <p>{eventTimeStamp}</p>
+      <p>{eventDescription}</p>
+
     </div>
   )
 }
